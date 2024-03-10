5973f6ab4157c3c7ce0c8682db1f083c2d3558d7
Bruno Alves <alves.bruno945@gmail.com>
2024-03-10 16:57:53 -0300

    Create the cell

--- AppetizerListView.swift
+++ AppetizerListView.swift
@@ -4,7 +4,23 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationView {
             List(MockData.appetizer, id: \.id) { appetizer in
-                Text(appetizer.name)
+                HStack {
+                    Image("food")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .clipShape(.rect(cornerRadius: 8))
+
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+
+                        Text("R$ \(appetizer.price, specifier: "%.2f")")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                }
             }
             .navigationTitle("🍟 Appetizers")
         }
