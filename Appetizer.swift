a1ca53aba3dad81c4f00b4908333abb33c5dafa0
Bruno Alves <alves.bruno945@gmail.com>
2024-03-10 13:30:39 -0300

    Add struct `Appetizer`

---
+++ Appetizer.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Appetizer: Codable {
+
+    let imageURL: String
+    let name: String
+    let description: String
+    let protein: Int
+    let id: Int
+    let price: Double
+    let carbs: Double
+    let calories: Int
+}
