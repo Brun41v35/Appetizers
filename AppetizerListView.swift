ca7206d774077646c8af7a7769ea2b8a0d301d89
Bruno Alves <alves.bruno945@gmail.com>
2024-03-10 17:11:08 -0300

    Remove components cell and add AppetizerListCell

--- AppetizerListView.swift
+++ AppetizerListView.swift
@@ -4,24 +4,7 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationView {
             List(MockData.appetizer, id: \.id) { appetizer in
-                HStack {
-                    Image("food")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120, height: 90)
-                        .clipShape(.rect(cornerRadius: 8))
-
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(appetizer.name)
-                            .font(.title2)
-                            .fontWeight(.medium)
-
-                        Text("R$ \(appetizer.price, specifier: "%.2f")")
-                            .foregroundStyle(.secondary)
-                            .fontWeight(.semibold)
-                    }
-                    .padding(.leading)
-                }
+                AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
         }
