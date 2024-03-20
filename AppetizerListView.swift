fd8900fc0e0e04c4340f073eed19795195cc050e
Bruno Alves <alves.bruno945@gmail.com>
2024-03-19 21:06:53 -0300

    Add `state` property and call `NetworkManager`

--- AppetizerListView.swift
+++ AppetizerListView.swift
@@ -1,12 +1,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+
+    // MARK - State Properties
+
+    @State private var appetizer: [Appetizer] = []
+
+    // MARK: - Body
+
     var body: some View {
         NavigationView {
             List(MockData.appetizer, id: \.id) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }
+    }
+
+    // MARK: - Internal Methods
+
+    func getAppetizers() {
+        NetworkManager.shared.loadData { result in
+            switch result {
+            case .success(let appetizers):
+                print(appetizers)
+            case .failure(let failure):
+                print(failure)
+            }
         }
     }
 }
