78ac22215632eddb045b6edc4b60497ea22c1cc7
Bruno Alves <alves.bruno945@gmail.com>
2024-03-21 23:52:58 -0300

    Extract method `getAppetizers` to viewModel

--- AppetizerListViewModel.swift
+++ AppetizerListViewModel.swift
@@ -5,4 +5,19 @@ final class AppetizerListViewModel: ObservableObject {
     // MARK: - Properties
 
     @Published var appetizer: [Appetizer] = []
+
+    // MARK: - Internal Methods
+
+    func getAppetizers() {
+        NetworkManager.shared.loadData { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizer = appetizers
+                case .failure(let failure):
+                    print(failure)
+                }
+            }
+        }
+    }
 }
